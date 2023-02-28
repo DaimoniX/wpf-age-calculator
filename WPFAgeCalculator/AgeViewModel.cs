95765d7550be245ac276a8fa93f8325313e1f7a1
DaimoniX <dimaprotsap@gmail.com>
2023-02-28 19:26:42 +0200

    Added zodiac signs

--- WPFAgeCalculator/AgeViewModel.cs
+++ WPFAgeCalculator/AgeViewModel.cs
@@ -9,6 +9,8 @@ public class AgeViewModel : INotifyPropertyChanged
     public event PropertyChangedEventHandler? PropertyChanged;
     
     private int _age;
+    private string _westernZodiacSign = string.Empty;
+    private string _chineseZodiacSign = string.Empty;
     private DateTime _date = DateTime.Today;
 
     public int Age
@@ -16,6 +18,7 @@ public class AgeViewModel : INotifyPropertyChanged
         get => _age;
         private set
         {
+            if(value == _age) return;
             _age = value;
             OnPropertyChanged();
         }
@@ -26,8 +29,33 @@ public class AgeViewModel : INotifyPropertyChanged
         get => _date;
         set
         {
+            if (value == _date) return;
             _date = value;
             Age = (DateTime.Now - _date).Days / 365;
+            WesternZodiacSign = ZodiacSign.GetWesternSign(_date);
+            ChineseZodiacSign = ZodiacSign.GetChineseSign(_date);
+            OnPropertyChanged();
+        }
+    }
+    
+    public string WesternZodiacSign
+    {
+        get => _westernZodiacSign;
+        private set
+        {
+            if (value == _westernZodiacSign) return;
+            _westernZodiacSign = value ?? throw new ArgumentNullException(nameof(value));
+            OnPropertyChanged();
+        }
+    }
+
+    public string ChineseZodiacSign
+    {
+        get => _chineseZodiacSign;
+        private set
+        {
+            if (value == _chineseZodiacSign) return;
+            _chineseZodiacSign = value ?? throw new ArgumentNullException(nameof(value));
             OnPropertyChanged();
         }
     }
@@ -39,7 +67,7 @@ public class AgeViewModel : INotifyPropertyChanged
 
     public bool IsBirthdayToday()
     {
-        var now = DateTime.Now;
+        var now = DateTime.Today;
         return now.Month == Date.Month && now.Day == Date.Day;
     }
 
--- WPFAgeCalculator/MainWindow.xaml.cs
+++ WPFAgeCalculator/MainWindow.xaml.cs
@@ -1,35 +1,34 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 
-namespace WPFAgeCalculator
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        private readonly AgeViewModel _ageViewModel;
-        
-        public MainWindow()
-        {
-            InitializeComponent();
-            AgeText.Visibility = Visibility.Collapsed;
-            DataContext = _ageViewModel = new AgeViewModel();
-        }
+namespace WPFAgeCalculator;
 
-        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly AgeViewModel _ageViewModel;
+        
+    public MainWindow()
+    {
+        InitializeComponent();
+        InfoGroup.Visibility = Visibility.Collapsed;
+        DataContext = _ageViewModel = new AgeViewModel();
+    }
+
+    private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (_ageViewModel.IsValidBirthDate())
         {
-            if (_ageViewModel.IsValidBirthDate())
-            {
-                AgeText.Visibility = Visibility.Visible;
-                if (_ageViewModel.IsBirthdayToday())
-                    MessageBox.Show("Happy birthday!", "Congratulations", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-            else
-            {
-                AgeText.Visibility = Visibility.Collapsed;
-                MessageBox.Show("Age cannot be more than 135 or less than 0", "Invalid age", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            InfoGroup.Visibility = Visibility.Visible;
+            if (_ageViewModel.IsBirthdayToday())
+                MessageBox.Show("Happy birthday!", "Congratulations", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        else
+        {
+            InfoGroup.Visibility = Visibility.Collapsed;
+            MessageBox.Show("Age cannot be more than 135 or less than 0", "Invalid age", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
